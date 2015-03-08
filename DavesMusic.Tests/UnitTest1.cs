14d3a6f70e3383d714da8c50496770761ccd3ae8
Dave Mateer <davemateer@gmail.com>
2015-03-08 09:15:16 +0000

    Sample test project MSTest

---
+++ DavesMusic.Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DavesMusic.Tests {
+    [TestClass]
+    public class UnitTest1 {
+        [TestMethod]
+        public void Sample_PassingTest() {
+            Assert.AreEqual(1,1);
+        }
+
+        [TestMethod]
+        public void Sample_PassingTest2() {
+            Assert.AreNotEqual(1, 2);
+        }
+    }
+}
