4653a9b1e21daf6981b06b119c88b4df99e56763
Dave Mateer <davemateer@gmail.com>
2015-03-20 07:34:09 +0000

    Added error logging only using log4net.  Put on page titles

--- DavesMusic/Controllers/HomeController.cs
+++ DavesMusic/Controllers/HomeController.cs
@@ -13,85 +13,6 @@ using System.Web.Mvc;
 
 namespace DavesMusic.Controllers {
 
-    public class TrackSearchResponse {
-        public Tracks tracks { get; set; }
-
-        public class Tracks {
-            public string href { get; set; }
-            public Item[] items { get; set; }
-            public int limit { get; set; }
-            public string next { get; set; }
-            public int offset { get; set; }
-            public object previous { get; set; }
-            public int total { get; set; }
-        }
-
-        public class Item {
-            public Album album { get; set; }
-            public Artist[] artists { get; set; }
-            public string[] available_markets { get; set; }
-            public int disc_number { get; set; }
-            public int duration_ms { get; set; }
-            public bool _explicit { get; set; }
-            public External_Ids external_ids { get; set; }
-            public External_Urls1 external_urls { get; set; }
-            public string href { get; set; }
-            public string id { get; set; }
-            public string name { get; set; }
-            public int popularity { get; set; }
-            public string preview_url { get; set; }
-            public int track_number { get; set; }
-            public string type { get; set; }
-            public string uri { get; set; }
-        }
-
-        public class Album {
-            public string album_type { get; set; }
-            public string[] available_markets { get; set; }
-            public External_Urls external_urls { get; set; }
-            public string href { get; set; }
-            public string id { get; set; }
-            public Image[] images { get; set; }
-            public string name { get; set; }
-            public string type { get; set; }
-            public string uri { get; set; }
-        }
-
-        public class External_Urls {
-            public string spotify { get; set; }
-        }
-
-        public class Image {
-            public int height { get; set; }
-            public string url { get; set; }
-            public int width { get; set; }
-        }
-
-        public class External_Ids {
-            public string isrc { get; set; }
-        }
-
-        public class External_Urls1 {
-            public string spotify { get; set; }
-        }
-
-        public class Artist {
-            public External_Urls2 external_urls { get; set; }
-            public string href { get; set; }
-            public string id { get; set; }
-            public string name { get; set; }
-            public string type { get; set; }
-            public string uri { get; set; }
-        }
-
-        public class External_Urls2 {
-            public string spotify { get; set; }
-        }
-    }
-
-   
-
-
     public class HomeController : Controller {
         string connectionString = ConfigurationManager.ConnectionStrings["DavesMusicConnection2"].ConnectionString;
 
@@ -247,6 +168,10 @@ namespace DavesMusic.Controllers {
             return View(a);
         }
 
+
+        public ActionResult Error(){
+            return View();
+        }
         public ActionResult TopTracks() {
             var vm = new List<SongsVM>();
             using (var connection = new SqlConnection(connectionString))
@@ -324,6 +249,10 @@ namespace DavesMusic.Controllers {
                 }
             }
             return View(vm);
+        }
+
+        public ActionResult About(){
+            return View();
         }
 
         public ActionResult LoginRedirect() {
@@ -644,6 +573,82 @@ namespace DavesMusic.Controllers {
                 public string Url { get; set; }
                 public int Width { get; set; }
             }
+        }
+    }
+
+    public class TrackSearchResponse {
+        public Tracks tracks { get; set; }
+
+        public class Tracks {
+            public string href { get; set; }
+            public Item[] items { get; set; }
+            public int limit { get; set; }
+            public string next { get; set; }
+            public int offset { get; set; }
+            public object previous { get; set; }
+            public int total { get; set; }
+        }
+
+        public class Item {
+            public Album album { get; set; }
+            public Artist[] artists { get; set; }
+            public string[] available_markets { get; set; }
+            public int disc_number { get; set; }
+            public int duration_ms { get; set; }
+            public bool _explicit { get; set; }
+            public External_Ids external_ids { get; set; }
+            public External_Urls1 external_urls { get; set; }
+            public string href { get; set; }
+            public string id { get; set; }
+            public string name { get; set; }
+            public int popularity { get; set; }
+            public string preview_url { get; set; }
+            public int track_number { get; set; }
+            public string type { get; set; }
+            public string uri { get; set; }
+        }
+
+        public class Album {
+            public string album_type { get; set; }
+            public string[] available_markets { get; set; }
+            public External_Urls external_urls { get; set; }
+            public string href { get; set; }
+            public string id { get; set; }
+            public Image[] images { get; set; }
+            public string name { get; set; }
+            public string type { get; set; }
+            public string uri { get; set; }
+        }
+
+        public class External_Urls {
+            public string spotify { get; set; }
+        }
+
+        public class Image {
+            public int height { get; set; }
+            public string url { get; set; }
+            public int width { get; set; }
+        }
+
+        public class External_Ids {
+            public string isrc { get; set; }
+        }
+
+        public class External_Urls1 {
+            public string spotify { get; set; }
+        }
+
+        public class Artist {
+            public External_Urls2 external_urls { get; set; }
+            public string href { get; set; }
+            public string id { get; set; }
+            public string name { get; set; }
+            public string type { get; set; }
+            public string uri { get; set; }
+        }
+
+        public class External_Urls2 {
+            public string spotify { get; set; }
         }
     }
 }
--- DavesMusic/Global.asax.cs
+++ DavesMusic/Global.asax.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using log4net;
 
 namespace DavesMusic
 {
@@ -12,10 +13,33 @@ namespace DavesMusic
     {
         protected void Application_Start()
         {
+            log4net.Config.XmlConfigurator.Configure();
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        void Application_Error(Object sender, EventArgs e) {
+            var exception = Server.GetLastError();
+            if (exception == null)
+                return;
+            ILog log = LogManager.GetLogger(typeof(MvcApplication));
+            log.Error(exception.Message);
+            log.Error(exception.StackTrace);
+            //var mail = new MailMessage { From = new MailAddress("automated@contoso.com") };
+            //mail.To.Add(new MailAddress("administrator@contoso.com"));
+            //mail.Subject = "Site Error at " + DateTime.Now;
+            //mail.Body = "Error Description: " + exception.Message;
+            //var server = new SmtpClient { Host = "your.smtp.server" };
+            //server.Send(mail);
+
+            // Clear the error
+            Server.ClearError();
+
+            // Redirect to a landing page
+            Response.Redirect("/home/error");
         }
     }
 }
