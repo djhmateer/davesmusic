9d9a7ec383250e8582acc68bd62c83be2593bc64
Dave Mateer <davemateer@gmail.com>
2016-06-22 12:05:41 +0100

    linker time

--- DavesMusic/Controllers/HomeController.cs
+++ DavesMusic/Controllers/HomeController.cs
@@ -7,14 +7,49 @@ using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
 using System.Diagnostics;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Reflection;
 using System.Web.Mvc;
 
 namespace DavesMusic.Controllers {
+    public static class Helpers
+    {
+        //http://stackoverflow.com/questions/1600962/displaying-the-build-date
+        public static String GetLinkerTime(this Assembly assembly, TimeZoneInfo target = null)
+        {
+            var filePath = assembly.Location;
+            const int c_PeHeaderOffset = 60;
+            const int c_LinkerTimestampOffset = 8;
+
+            var buffer = new byte[2048];
+
+            using (var stream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+                stream.Read(buffer, 0, 2048);
+
+            var offset = BitConverter.ToInt32(buffer, c_PeHeaderOffset);
+            var secondsSince1970 = BitConverter.ToInt32(buffer, offset + c_LinkerTimestampOffset);
+            var epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+            var linkTimeUtc = epoch.AddSeconds(secondsSince1970);
+
+            var tz = target ?? TimeZoneInfo.Local;
+            var localTime = TimeZoneInfo.ConvertTimeFromUtc(linkTimeUtc, tz);
+
+            // build done on west coast USA (Appveyor)
+            // Server is in Ireland so want it to display UK Time.
+            var britishZone = TimeZoneInfo.FindSystemTimeZoneById("GMT Standard Time");
+            //var linkerTime = Assembly.GetExecutingAssembly().GetLinkerTime();
+            var newDate = TimeZoneInfo.ConvertTime(localTime, TimeZoneInfo.Local, britishZone);
+
+            return newDate.ToString("dd/MM/yyyy HH:mm:ss", CultureInfo.InvariantCulture); ;
+        }
+    }
+
     public class HomeController : Controller {
         string connectionString = ConfigurationManager.ConnectionStrings["DavesMusicConnection2"].ConnectionString;
         MiniProfiler mp = MiniProfiler.Current;
